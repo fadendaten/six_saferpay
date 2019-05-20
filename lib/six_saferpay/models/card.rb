5123e3f01265ed331308b54e7f0e5b1b2844ddcb
Felix Langenegger <felix.langenegger@fadendaten.ch>
2019-05-20 20:39:00 +0200

    Downcase hash keys in card

--- lib/six_saferpay/models/card.rb
+++ lib/six_saferpay/models/card.rb
@@ -26,12 +26,12 @@ module SixSaferpay
 
     def to_hash
       body = Hash.new
-      body.merge!(MaskedNumber: @masked_number)
-      body.merge!(ExpYear: @exp_year)
-      body.merge!(ExpMonth: @exp_month)
-      body.merge!(HolderName: @holder_name) if @holder_name
-      body.merge!(CountryCode: @country_code) if @country_code
-      body.merge!(HashValue: @hash_value) if @hash_value
+      body.merge!(masked_number: @masked_number)
+      body.merge!(exp_year: @exp_year)
+      body.merge!(exp_month: @exp_month)
+      body.merge!(holder_name: @holder_name) if @holder_name
+      body.merge!(country_code: @country_code) if @country_code
+      body.merge!(hash_value: @hash_value) if @hash_value
       body
     end
     alias_method :to_h, :to_hash
--- lib/six_saferpay/version.rb
+++ lib/six_saferpay/version.rb
@@ -1,3 +1,3 @@
 module SixSaferpay
-  VERSION = "0.1.4"
+  VERSION = "0.1.6"
 end
