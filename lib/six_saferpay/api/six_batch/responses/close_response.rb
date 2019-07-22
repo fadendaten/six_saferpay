9fe7690a3d20cc5d6d04c24895ccc80b2f5861ec
Felix Langenegger <felix.langenegger@fadendaten.ch>
2019-07-22 16:39:48 +0200

    New Version 2.0.1

--- lib/six_saferpay/api/six_batch/responses/close_response.rb
+++ lib/six_saferpay/api/six_batch/responses/close_response.rb
@@ -5,12 +5,12 @@ module SixSaferpay
       attr_accessor(:response_header)
 
       def initialize(response_header:)
-        @response_header = SixSaferpay::ResponseHeader.new(response_header.to_h) if response_header
+        @response_header = SixSaferpay::ResponseHeader.new(response_header.to_h)
       end
 
       def to_hash
         hash = Hash.new
-        hash.merge!(response_header: @response_header.to_h) if @response_header
+        hash.merge!(response_header: @response_header.to_h)
         hash
       end
       alias_method :to_h, :to_hash
