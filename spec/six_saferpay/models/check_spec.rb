7f351d9b8f87cefe9934c758dac0b6e22124825c
Felix Langenegger <felix.langenegger@gmail.com>
2021-02-14 14:40:44 +0100

    Added spec for 'ONLINE_STRONG' in the check models.

--- spec/six_saferpay/models/check_spec.rb
+++ spec/six_saferpay/models/check_spec.rb
@@ -2,18 +2,38 @@ require 'spec_helper'
 
 RSpec.describe SixSaferpay::Check do
 
-  subject { SpinningWheel.create('check') }
+  describe 'online check type' do
 
-  let(:hash) {
-    {
-      type: subject.type,
-      terminal_id: subject.terminal_id
+    subject { SpinningWheel.create('check') }
+
+    let(:hash) {
+      {
+        type: subject.type,
+        terminal_id: subject.terminal_id
+      }
     }
-  }
 
-  describe 'to_hash' do
-    it 'returns the hash representation of the check' do
-      expect(subject.to_hash).to eq(hash)
+    describe 'to_hash' do
+      it 'returns the hash representation of the check' do
+        expect(subject.to_hash).to eq(hash)
+      end
+    end
+  end
+
+  describe 'online strong check type' do
+    subject { SpinningWheel.create('check_online_strong') }
+
+    let(:hash) {
+      {
+        type: subject.type,
+        terminal_id: subject.terminal_id
+      }
+    }
+
+    describe 'to_hash' do
+      it 'returns the hash representation of the check' do
+        expect(subject.to_hash).to eq(hash)
+      end
     end
   end
 end
